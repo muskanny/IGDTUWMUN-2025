065fe5d93e168ada6cd4e4c079fedac18a008370
Muskan Singh <muskan.s.del@gmail.com>
2024-12-08 11:14:22 +0530

    past events

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,29 +1,4 @@
-// Carousel JS
 
-window.onload = function () {
-    let slides = document.getElementsByClassName("carousel-item");
-    function addActive(slide) {
-      slide.classList.add("active");
-    }
-    function removeActive(slide) {
-      slide.classList.remove("active");
-    }
-    addActive(slides[0]);
-    setInterval(function () {
-      for (let i = 0; i < slides.length; i++) {
-        if (i + 1 == slides.length) {
-          addActive(slides[0]);
-          setTimeout(removeActive, 350, slides[i]);
-          break;
-        }
-        if (slides[i].classList.contains("active")) {
-          setTimeout(removeActive, 350, slides[i]);
-          addActive(slides[i + 1]);
-          break;
-        }
-      }
-    }, 2000);
-  };
   
   // Core JS
   function isElementInViewport(el) {
@@ -49,6 +24,7 @@ window.onload = function () {
       }
     });
   }
+  
   
   // Attach an event listener for the scroll event
   window.addEventListener("scroll", handleScroll);
